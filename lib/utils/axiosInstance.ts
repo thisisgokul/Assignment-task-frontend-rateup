db68871542d3a10a2e9ff166e9f2d404ed2cd887
thisisgokul <gokulsubashwydnov@gmail.com>
2025-03-18 17:48:14 +0530

    updated finally

--- lib/utils/axiosInstance.ts
+++ lib/utils/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create Axios instance with base URL
 const axiosInstance = axios.create({
-  baseURL: ' http://localhost:5000/api', 
+  baseURL: ' https://assignment-task-backend-rateup.vercel.app/api', 
 });
 
 export default axiosInstance;
